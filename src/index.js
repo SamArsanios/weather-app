63af1ec7ca2f7bb7a36533c5e79f80e18d2fc22b
SamArsanios <samson.kibrom@hotmail.com>
2021-05-04 20:10:43 +0300

    Modify search weather

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
+import * as weatherData from './weatherData';
+import view from './view';
 import './styles.css';
+
+const weatherLocation = function () {
+  view.showSpinner();
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(loadLocation, function () {
+      view.showMessage();
+    });
+  }
+};
+
+
+const loadLocation = async function (position) {
+  try {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    // Load location weather
+    await weatherData.loadWeather([latitude, longitude])
+
+    // Render weather data
+    view.renderWeather(weatherData.weather.data);
+  } catch (err) {
+    console.error(err);
+    view.showMessage(err.message);
+  }
+};
+
+const searchWeather = async function (city) {
+  try {
+    // console.log(city);
+    view.showSpinner();
+    // Get query from search input
+    // const query = view.getWeather();
+    if (!city) return;
+
+    // Load weather based on search query
+    await weatherData.loadWeather(city);
+
+    // Render weather data
+    view.renderWeather(weatherData.weather.data);
+  } catch (err) {
+    console.error(err);
+    view.showMessage(err.message);
+  }
+};
+
+const convert = function (unit) {
+  weatherData.convertUnit(unit);
+  view.renderWeather(weatherData.weather.data);
+};
+
+const init = function () {
+  view.addHandlerRender(weatherLocation);
+  view.addHandlerSearch(searchWeather);
+  view.addHandlerConvert(convert);
+};
+
+init();
