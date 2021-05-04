9a5bcaf1b247cf6dce0f0901d0901ab16daefeba
SamArsanios <samson.kibrom@hotmail.com>
2021-05-04 20:12:00 +0300

    Add country API url

--- src/configuration.js
+++ src/configuration.js
@@ -1,5 +1,6 @@
 export const KEY = "7f84933d96924f1148daa50fdda85e85";
 export const API_URL = "https://api.openweathermap.org/data/2.5/weather?";
+export const REST_COUNTRY_URL = "https://restcountries.eu/rest/v2/alpha?codes=";
 export const UNIT = "metric";
 export const TIMEOUT_SEC = 10;
 
@@ -16,6 +17,7 @@ export const AJAX = async function (url) {
         const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
         const data = await res.json();
 
+        console.log(data);
         if (!res.ok) {
             throw new Error(
                 `${data.message[0].toUpperCase() + data.message.slice(1)} (${res.status
