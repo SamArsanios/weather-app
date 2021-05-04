bfac14dbee38b5a4e07f3625b90ec5724ab1d95f
SamArsanios <samson.kibrom@hotmail.com>
2021-05-04 11:30:30 +0300

    Add API configuration

---
+++ src/configuration.js
@@ -0,0 +1,29 @@
+export const KEY = "7f84933d96924f1148daa50fdda85e85";
+export const API_URL = "https://api.openweathermap.org/data/2.5/weather?";
+export const UNIT = "metric";
+export const TIMEOUT_SEC = 10;
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+
+export const AJAX = async function (url) {
+    try {
+        const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(
+                `${data.message[0].toUpperCase() + data.message.slice(1)} (${res.status
+                })`
+            );
+        }
+        return data;
+    } catch (err) {
+        throw err;
+    }
+};
